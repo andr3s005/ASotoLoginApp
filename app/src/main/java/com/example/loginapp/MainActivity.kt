0bd348b74b8e95464a3d9931a5de5f95ad83a7d5
andr3s005 <asg78886@lasallebajio.edu.mx>
2025-09-26 18:49:55 -0600

    Modificación de MainActivity.kt

--- app/src/main/java/com/example/loginapp/MainActivity.kt
+++ app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -6,12 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import com.example.loginapp.ui.theme.LoginAppTheme
+import com.example.loginapp.ui.theme.Register
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +25,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LoginAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = LoginScreen
+                    ){
+                        composable<LoginScreen>{
+                            LoginScreen(
+                                onNavigateToRegister = {
+                                    navController.navigate(RegisterScreen)
+                                }
+                            )
+                        }
+
+                        composable<RegisterScreen>{
+                            RegisterScreen(
+                                onBack = {
+                                    navController.popBackStack()
+                                }
+                            )
+                        }
+                    }
                 }
             }
         }
